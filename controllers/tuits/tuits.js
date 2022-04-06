23e4e9161c55098d61788a6b1e8d227003c300ca
毛玥琪 <maoyueqi@kes-MacBook-Pro.local>
2022-04-06 00:46:30 -0400

    first commit

---
+++ controllers/tuits/tuits.js
@@ -0,0 +1,17 @@
+export default [
+    {
+        "_id": "123",
+        "postedBy": {"username": "NASA"},
+        "tuit": "In 2021, our @NASAPersevere Mars rover landed",
+        "likes": 345,
+        "comments": 123,
+        "retuits": 234,
+
+    },
+    {
+        "_id": "234",
+        "postedBy": { "username": "NASA" },
+        "tuit": "Ingenuity helicopter takes flight.",
+        "likes": 346785
+    }
+];
